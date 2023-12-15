0aee5c936c23a4edbe846dfc1f34b8f51d65e0a0
Andrew Stellwag <aestellwag@gmail.com>
2023-12-15 11:10:10 -0500

    End Chat Button Updated to Park Interactions

--- plugin-flex-ts-template-v2/src/feature-library/park-interaction/flex-hooks/components/TaskCanvasHeader.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/park-interaction/flex-hooks/components/TaskCanvasHeader.tsx
@@ -5,6 +5,8 @@ import { FlexComponent } from '../../../../types/feature-loader';
 
 export const componentName = FlexComponent.TaskCanvasHeader;
 export const componentHook = function addParkButton(flex: typeof Flex) {
+  // This also removes end task for all types, may need to add this back later after testing
+  flex.TaskCanvasHeader.Content.remove('actions');
   flex.TaskCanvasHeader.Content.add(<ParkButton key="park-button" />, {
     sortOrder: 1,
     if: (props) => Flex.TaskHelper.isCBMTask(props.task) && props.task.taskStatus === 'assigned',
