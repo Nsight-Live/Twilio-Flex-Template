7d96d4bf3ec9446165483586c8b92cce332db5eb
Andrew Stellwag <aestellwag@gmail.com>
2024-03-01 14:44:29 -0500

    Added an enhancement to allow agents to edit the dealer name attribute

--- plugin-flex-ts-template-v2/src/feature-library/enhanced-agent-task-attributes/custom-components/EnhancedAgentTaskDetails/EnchancedAgentTaskDetails.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/enhanced-agent-task-attributes/custom-components/EnhancedAgentTaskDetails/EnchancedAgentTaskDetails.tsx
@@ -2,44 +2,55 @@ import * as React from 'react';
 import { ITask } from '@twilio/flex-ui';
 import { Flex } from '@twilio-paste/core/flex';
 import { Text } from '@twilio-paste/core/text';
+import { Button } from '@twilio-paste/core/button';
+import { Input } from '@twilio-paste/core/input';
+import { EditIcon } from '@twilio-paste/icons/esm/EditIcon';
+import { AcceptIcon } from '@twilio-paste/icons/esm/AcceptIcon';
 
 type EnhancedAgentTaskDetailsProps = {
   task: ITask;
 };
 
 const EnhancedAgentTaskDetails = ({ task }: EnhancedAgentTaskDetailsProps) => {
-  if (task.attributes.dealerName) {
-    return (
-      <Flex hAlignContent="center" vertical>
+  const [dealerName, setDealerName] = React.useState(task.attributes.dealerName || ''); // default empty string
+  const [editMode, setEditMode] = React.useState(false); // editMode state
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault(); // to prevent page refresh when the form is submitted
+    task.setAttributes({ ...task.attributes, dealerName }); // update task attributes
+    setEditMode(false); // switch off edit mode
+  };
+
+  return (
+    <Flex hAlignContent="center" vertical>
+      {/* Wrap Dealer name and Edit button in another Flex component */}
+      <Flex hAlignContent="between">
         <Text
           as="p"
-          textAlign="center"
           fontWeight="fontWeightMedium"
           fontSize="fontSize30"
           marginTop="space20"
           marginBottom="space20"
+          paddingRight="space20"
           color="colorTextSuccess"
         >
-          Dealer Name: {task.attributes.dealerName}
+          Dealer Name: {dealerName || 'Unknown'}
         </Text>
+        <Button variant="primary_icon" size="reset" onClick={() => setEditMode(true)}>
+          <EditIcon decorative={false} title="Edit" />
+        </Button>
       </Flex>
-    );
-  }
-  return (
-    <Flex hAlignContent="center" vertical>
-      <Text
-        as="p"
-        textAlign="center"
-        fontWeight="fontWeightMedium"
-        fontSize="fontSize30"
-        marginTop="space20"
-        marginBottom="space20"
-        color="colorTextSuccess"
-      >
-        Dealer Name: Unknown
-      </Text>
+      {editMode && (
+        <form onSubmit={handleSubmit}>
+          <Flex hAlignContent="between" marginBottom="space20">
+            <Input id="dealerName" type="text" value={dealerName} onChange={(e) => setDealerName(e.target.value)} />
+            <Button variant="primary" type="submit">
+              <AcceptIcon decorative={false} title="Accept" />
+            </Button>
+          </Flex>
+        </form>
+      )}
     </Flex>
   );
 };
-
 export default EnhancedAgentTaskDetails;
